169eb787092119b0177027465d07b3e01581ae5d
mishrasaurav <63809043+mishrasaurav@users.noreply.github.com>
2020-07-28 09:18:44 +0530

    Create LargestNumberInArray.cpp

---
+++ basicquestions/LargestNumberInArray.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    int temp;
+    int *arr = new int[n];
+    
+    for(int i=0;i<n;i++){
+        
+        cin>>arr[i];
+    }
+    
+    for(int i=1;i<n;i++){
+        if(arr[0]>arr[i])
+        {
+            arr[0]=arr[i];
+        }
+        
+    }
+    
+    cout<<arr[0];
+    
+    return 0;
+}
